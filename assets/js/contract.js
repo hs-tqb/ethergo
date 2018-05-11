3d081830d36675639bcefbc08f7fc76eeb5e7047
HS <huangshuai@utianqi.com>
2018-05-11 13:50:00 +0800

    改地址

--- assets/js/contract.js
+++ assets/js/contract.js
@@ -5,7 +5,7 @@ export default {
     // '0xba34f675c6e440333754e11ef8c1c953d9fa8f20':  // 记录测试
     '0xb0a0968b6cff27d4b0abb576f473076b49b3f4b6': // 新版
     // '0x29f81c70b35c5cc84ac6deaf22c7221eec56fde6': // 可提现金额测试
-    '0x4DF6DE08D11f11EBAd5d9E136B768849426fB8a7',
+    '0x2D05359A51ca13C4ac5f4437585AFaf5bF2050F9',
 
     abi: abi
 }
