6a3058b1b54b456b61b71a4e88190406ed97772b
HS <huangshuai@utianqi.com>
2018-05-12 15:33:39 +0800

    恢复主网合约

--- assets/js/contract.js
+++ assets/js/contract.js
@@ -7,10 +7,9 @@ export default {
     // 
 
     // 正式版
-    // '0x2D05359A51ca13C4ac5f4437585AFaf5bF2050F9',
+    '0x2D05359A51ca13C4ac5f4437585AFaf5bF2050F9',
     // '0xb0a0968b6cff27d4b0abb576f473076b49b3f4b6',
-
-    '0xe7fca0faf2636a4bbd5143d4f6e0667943ed9694',
+    // '0xe7fca0faf2636a4bbd5143d4f6e0667943ed9694',
 
     abi: abi
 }
