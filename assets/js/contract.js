6dc3302af2ec2b68f3648cd06e877967c4d02ea1
HS <huangshuai@utianqi.com>
2018-05-12 14:16:18 +0800

    正式版跑测试合约

--- assets/js/contract.js
+++ assets/js/contract.js
@@ -1,11 +1,12 @@
 import abi from './abi'
 export default {
   address:process.env.NODE_ENV==='development'? 
-    // '0xa0201395f7cde5b9dc39857b949ae8c2004eaf61':
-    // '0xba34f675c6e440333754e11ef8c1c953d9fa8f20':  // 记录测试
+    // 测试
     '0xb0a0968b6cff27d4b0abb576f473076b49b3f4b6': // 新版
-    // '0x29f81c70b35c5cc84ac6deaf22c7221eec56fde6': // 可提现金额测试
-    '0x2D05359A51ca13C4ac5f4437585AFaf5bF2050F9',
+
+    // 正式版
+    // '0x2D05359A51ca13C4ac5f4437585AFaf5bF2050F9',
+    '0xb0a0968b6cff27d4b0abb576f473076b49b3f4b6',
 
     abi: abi
 }
