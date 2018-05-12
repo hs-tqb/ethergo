794cc12ff934de43a436951ff055376b8e8cb6a1
HS <huangshuai@utianqi.com>
2018-05-12 14:25:54 +0800

    换回正式版合约地址

--- assets/js/contract.js
+++ assets/js/contract.js
@@ -5,8 +5,8 @@ export default {
     '0xb0a0968b6cff27d4b0abb576f473076b49b3f4b6': // 新版
 
     // 正式版
-    // '0x2D05359A51ca13C4ac5f4437585AFaf5bF2050F9',
-    '0xb0a0968b6cff27d4b0abb576f473076b49b3f4b6',
+    '0x2D05359A51ca13C4ac5f4437585AFaf5bF2050F9',
+    // '0xb0a0968b6cff27d4b0abb576f473076b49b3f4b6',
 
     abi: abi
 }
