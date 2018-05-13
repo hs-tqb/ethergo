0efbbb4afc1d0963b5e103362173be49170dae47
hs <35157375@qq.com>
2018-05-13 12:26:28 +0800

    隐藏正式环境上的debug, 并创建一个用以debug的页面

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,12 +11,12 @@
     .btn.block { height:52px; font-size:22px; }
     .text-pending { .text-warning; }
   }
-      
+
 
   @import url(~assets/css/icons/success.less);
   @import url(~assets/css/icons/failure.less);
-  .dialog-container { 
-    position:fixed; top:0; left:0; z-index:999; width:100vw; height:100%; 
+  .dialog-container {
+    position:fixed; top:0; left:0; z-index:999; width:100vw; height:100%;
     background-color: rgba(0,0,0,.7);
     transition-duration:300ms;
     pointer-events:none; opacity:0;
@@ -26,19 +26,19 @@
     .flow; align-items:center; justify-content:center;
     background-color:transparent;
     &.show{ pointer-events:none; }
-    .inner-wrapper { 
+    .inner-wrapper {
       display:inline-block;
       margin-top:-150px;
-      padding:14px; 
+      padding:14px;
       max-width:70%;
       line-height:1.4;
       text-align:justify;
       color:#fff;
       background:rgba(51,51,51,.9) no-repeat center 12px;
-      background-size:30px auto ; 
+      background-size:30px auto ;
       box-shadow:0 0 3px 1px #777;
-      border-radius:8px; 
-      
+      border-radius:8px;
+
       &[data-type] {
         padding-top:50px;
         &:empty { background-position:center 10px; padding-bottom:0; }
@@ -49,7 +49,7 @@
       &[data-type=failure] { background-image:url(@icon-failure); }
     }
   }
-  #menu { 
+  #menu {
     position:fixed; top:55px; left:0; z-index:1000;
     width:100%;
     overflow:hidden;
@@ -59,20 +59,20 @@
       pointer-events:none;
       ul { transform:translate3d(0,-100%,0);  }
     }
-    ul { .flow(row); flex-wrap:wrap; padding:20px 20px 50px 20px; background:@color-primary; transition-duration:300ms; } 
-    li { 
+    ul { .flow(row); flex-wrap:wrap; padding:20px 20px 50px 20px; background:@color-primary; transition-duration:300ms; }
+    li {
       width:33%; line-height:50px; text-align:center; cursor:pointer;
-      a { 
-        display:block; margin:0 35px; 
-        .border(bottom); 
-        color:@color-white; 
+      a {
+        display:block; margin:0 35px;
+        .border(bottom);
+        color:@color-white;
         text-decoration:none;
         &:hover { color:@color-highlight; }
       }
     }
   }
 
-  #footer ul { 
+  #footer ul {
     .flow(row); justify-content:center; align-items:center; line-height:50px;
     li { position:relative; margin:0 10px; cursor:pointer; }
     li:not(:last-child)::after { position:absolute; right:-12px; top:24px; width:4px; height:4px; .radius; background-color:@color-white; content:'\200B'; }
@@ -80,21 +80,21 @@
   }
 
   @media screen and (max-width:411px) {
-    #menu { 
+    #menu {
       top:0; height:100%;
       display:flex;
       justify-content:center;
       align-items:center;
       background:@color-primary;
       ul { padding-bottom:20px; }
-      li { 
-        width:100%; height:100%; 
+      li {
+        width:100%; height:100%;
         a { border:0 none; }
       }
     }
     #footer { position:absolute; top:100%; left:0; width:100%;  }
   }
-  
+
 </style>
 
 <template>
@@ -155,12 +155,11 @@ export default {
     // 测试环境下的移动端页面, 加载debug工具
     if ( process.env.NODE_ENV === 'development' && (/iphone|ios|android|ipad/gi).test(navigator.userAgent) ) {
       localStorage.removeItem('hasReadGuide')
-      
-    }
       let script = document.createElement('script');
       script.onload = function() { eruda.init(); }
       document.body.appendChild(script)
       script.src = '//cdn.bootcss.com/eruda/1.4.2/eruda.min.js'
+    }
 
     this.hash = location.hash
   }
