2a5c3fa50b60e26c5caa0653716a22e6003582a4
HS <huangshuai@utianqi.com>
2018-05-18 21:06:46 +0800

    ga

--- pages/index.vue
+++ pages/index.vue
@@ -1118,7 +1118,7 @@ export default {
 
       // 投注
       // let contract = this.getContract();
-      let contract = this.web3.eth.contract(contracts.abi).at(contracts.addrs[bet.index])
+      let contract = this.web3.eth.contract(contracts.abi).at(contracts.addrs[this.bet.index])
       contract.userRollDice(+this.computedUserNumber, additionParam, (err, hash)=>{
         if ( err ) return this.commonErrorCatcher('已取消支付');
         let LogBet = contract.LogBet();
